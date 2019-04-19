40f5208df3925981031e5e717744b3c636a10691
Mateusz <1b13ostalecki@gmail.com>
2019-04-19 21:36:21 +0200

    Add QuizCreate endpoint This endpoint handles post requests with quiz data

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("quizzes/", views.QuizList.as_view(), name="quiz-list"),
-    path("quizzes/<int:pk>/", views.QuizDetail.as_view(), name="quiz-detail")
+    path("quizzes/<int:pk>/", views.QuizDetail.as_view(), name="quiz-detail"),
+    path("quizzes/create/", views.QuizCreate.as_view(), name="quiz-create")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -3,10 +3,14 @@ from rest_framework import generics
 from .models import Quiz, Question
 from .serializers import QuizSerializer, QuizListSerializer
 
-class QuizList(generics.ListCreateAPIView):
+class QuizList(generics.ListAPIView):
     queryset = Quiz.objects.all()
     serializer_class = QuizListSerializer
 
 class QuizDetail(generics.RetrieveAPIView):
 	queryset = Quiz.objects.all()
 	serializer_class = QuizSerializer
+
+class QuizCreate(generics.CreateAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
