e55e1df2df405dd0f161584bf1c783395b158876
Mateusz <1b13ostalecki@gmail.com>
2019-07-21 22:48:20 +0200

    Update README

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("quizzes/", views.QuizList.as_view(), name="quiz-list"),
     path("quizzes/<int:pk>/", views.QuizDetail.as_view(), name="quiz-detail"),
-    path("quizzes/create/", views.QuizCreate.as_view(), name="quiz-create")
+    path("quizzes/create/", csrf_exempt(views.QuizCreate.as_view()), name="quiz-create")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
