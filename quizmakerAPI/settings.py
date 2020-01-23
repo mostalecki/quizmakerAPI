28387775a38d9dc49530d20d606fa69dc552c12a
Mateusz <1b13ostalecki@gmail.com>
2020-01-24 00:16:05 +0100

    Update requirements.txt

--- quizmakerAPI/settings.py
+++ quizmakerAPI/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'siemanko'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2']
 
 
 # Application definition
